42783fa0e8965333d083a3c47d1b2376c3b58c6a
Blake Ball <blakesteve@gmail.com>
2026-03-11 19:12:06 -0500

    fix: build config

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,51 @@
 import './index.css';
 
 // --- ATOMS ---
-
-// Avatar
 export * from './components/atoms/Avatar/Avatar';
 export * from './components/atoms/Avatar/avatar-variants';
-
-// Badge
 export * from './components/atoms/Badge/Badge';
 export * from './components/atoms/Badge/badge-variants';
-
-// Button
 export * from './components/atoms/Button/Button';
 export * from './components/atoms/Button/button-variants';
-
-// Card
 export * from './components/atoms/Card/Card';
 export * from './components/atoms/Card/card-variants';
-
-// Disclosure
 export * from './components/atoms/Disclosure/Disclosure';
 export * from './components/atoms/Disclosure/disclosure-variants';
-
-// Input
 export * from './components/atoms/Input/Input';
 export * from './components/atoms/Input/input-variants';
-
-// Link
 export * from './components/atoms/Link/Link';
 export * from './components/atoms/Link/link-variants';
-
-// Select
 export * from './components/atoms/Select/Select';
 export * from './components/atoms/Select/select-variants';
-
-// Spinner (No variants file)
 export * from './components/atoms/Spinner/Spinner';
-
-// Switch
 export * from './components/atoms/Switch/Switch';
 export * from './components/atoms/Switch/switch-variants';
-
-// Textarea
 export * from './components/atoms/Textarea/Textarea';
 export * from './components/atoms/Textarea/textarea-variants';
 
 // --- MOLECULES ---
-
-// Accordion
 export * from './components/molecules/Accordion/Accordion';
-
-// Breadcrumbs
 export * from './components/molecules/Breadcrumbs/Breadcrumbs';
 export * from './components/molecules/Breadcrumbs/breadcrumb-variants';
-
-// CallToAction
 export * from './components/molecules/CallToAction/CallToAction';
 export * from './components/molecules/CallToAction/call-to-action-variants';
-
-// EmptyState
 export * from './components/molecules/EmptyState/EmptyState';
 export * from './components/molecules/EmptyState/empty-state-variants';
-
-// ErrorState
 export * from './components/molecules/ErrorState/ErrorState';
 export * from './components/molecules/ErrorState/error-state-variants';
-
-// MatchupCard
 export * from './components/molecules/MatchupCard/MatchupCard';
 
 // --- ORGANISMS ---
-
-// ActionBar
 export * from './components/organisms/ActionBar/ActionBar';
 export * from './components/organisms/ActionBar/action-bar-variants';
-
-// Countdown
 export * from './components/organisms/Countdown/Countdown';
-
-// Dialog
 export * from './components/organisms/Dialog/Dialog';
-
-// Footer
 export * from './components/organisms/Footer/Footer';
-
-// Navbar
 export * from './components/organisms/Navbar/Navbar';
 export * from './components/organisms/Navbar/navbar-variants';
 
-
 // --- HOOKS ---
-
 export * from './hooks/useCountdown';
 
-
 // --- UTILITIES ---
-
 export * from './lib/utils';
